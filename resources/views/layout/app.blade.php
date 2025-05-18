edda579c949a397f748257d1227ad893005f574f
zidnafaz <zidnafaza27@gmail.com>
2025-05-18 19:25:10 +0700

    Fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,12 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Dashboard')</title>
+    <title>
+        {{ config('adminlte.title', 'AdminLTE') }}
+        @hasSection('subtitle') | @yield('subtitle') @endif
+    </title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.css') }}">
     <!-- AdminLTE -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+
+    @stack('css')
+    <style>
+        .card-header { border-bottom: none; }
+        .card-title { font-weight: 600; }
+    </style>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -22,7 +33,16 @@
 
         <!-- Content Wrapper -->
         <div class="content-wrapper">
-            <!-- Main content -->
+            <!-- Content Header -->
+            @hasSection('content_header')
+                <div class="content-header">
+                    <div class="container-fluid">
+                        @yield('content_header')
+                    </div>
+                </div>
+            @endif
+
+            <!-- Main Content -->
             <section class="content pt-3">
                 <div class="container-fluid">
                     @yield('content')
@@ -36,23 +56,18 @@
 
     <!-- jQuery -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-
     <!-- Bootstrap 4 -->
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
+    <!-- AdminLTE -->
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <!-- AdminLTE App -->
-    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
-
-    <!-- jQuery Validation (CDNJS) -->
+    <!-- DataTables -->
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <!-- jQuery Validation -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"></script>
 
-    <!-- DataTables -->
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     @stack('scripts')
 </body>
 
